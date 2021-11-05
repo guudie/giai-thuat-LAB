292e1ff6935ba94bb312f8031ca66425130a3466
guudie <gottychaos@gmail.com>
2021-11-05 17:38:48 +0700

    added 3, 4

--- BTTH 1/2.cpp
+++ BTTH 1/2.cpp
@@ -2,10 +2,12 @@
 #include <fstream>
 #include <algorithm>
 #include <vector>
+#define inf (int)1e9
 using namespace std;
 
 vector<vector<int>> results;
 
+// hàm tính tổng các phần tử trong vector
 template<class T>
 T vectorSum(vector<T> v){
     T ans = 0;
@@ -14,21 +16,20 @@ T vectorSum(vector<T> v){
     return ans;
 }
 
+// giải thuật quay lui tìm số lượng xu nhỏ nhất đổi ra lượng 'remaining'
 int minExchange(int* const& coins, int cur, int* cnt, int n, int remaining){
-    if(remaining == 0){
-        vector<int> currentResult;
+    if(remaining == 0){                                 // nếu lượng còn lại = 0
+        vector<int> currentResult;                      // thêm vào tập các đáp án
         for(int i = 0; i < n; i++)
             currentResult.push_back(cnt[i]);
         results.push_back(currentResult);
         return 0;
     }
-    if(cur == n)
-        return 9999;
+    if(cur == n)                                        // giá trị mặc định khi remaining != 0
+        return inf;                                     // và con trỏ cur đã đi quá mảng coins
     
-    int ans = 9999;
-    for(int i = 0; i <= remaining/coins[cur]; i++){
-        if(remaining - i*coins[cur] < 0)
-            break;
+    int ans = inf;
+    for(int i = 0; i <= remaining/coins[cur]; i++){     // lặp qua những số lượng có thể cho coins[cur] và gọi đệ quy
         cnt[cur] = i;
         ans = min(ans, i + minExchange(coins, cur+1, cnt, n, remaining - i*coins[cur]));
     }
@@ -59,7 +60,10 @@ int main(){
     }
     fin >> k;
 
+    // lượng xu nhỏ nhất
     int ans = minExchange(coins, 0, cnt, n, k);
+
+    // lặp qua danh sách chứa lượng xu, tìm ra danh sách có lượng xu nhỏ nhất (= ans)
     for(int i = 0; i < results.size(); i++){
         if(vectorSum(results[i]) == ans){
             for(int j = 0; j < n; j++)
